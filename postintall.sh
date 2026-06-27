1a0411f08ce2dca048b43f8e775e416cf14abfe5
Lair Ortega Dehonor <lair.ortega@gmail.com>
2026-06-26 21:13:12 -0600

    Update postintall.sh

--- postintall.sh
+++ postintall.sh
@@ -18,7 +18,7 @@ echo "alias ll='ls -lh'" > ~/.bashrc.d/ll.conf
 echo "Settings environment set!"
 
 echo "Installing Docker dependencies."
-sudo apt-get install ca-certificates curl wget
+sudo apt install -y ca-certificates curl wget
 curl -fsSL https://get.docker.com/ -o get-docker.sh
 sudo sh ./get-docker.sh
 
@@ -30,17 +30,17 @@ echo "Docker dependencies done!"
 
 echo "Install Google Chrome"
 wget -O chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-sudo apt install ./chrome.deb
+sudo apt install -y ./chrome.deb
 echo "Google Chrome installed!"
 
 echo "Installing VS Code."
 wget -O vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
-sudo apt install ./vscode.deb -y
+sudo apt install ./vscode.deb
 echo "VS Code installed!"
 
 echo "Installing Dbeaver."
 wget -O dbeaver.deb 'https://dbeaver.io/files/dbeaver-ce-latest-linux-x86_64.deb'
-sudo apt install ./dbeaver.deb -y
+sudo apt install -y ./dbeaver.deb
 echo "Dbeaver installed!"
 
 echo "Installing nvm"
