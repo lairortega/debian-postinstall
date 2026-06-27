be76d1fc39c1666af237bf852ce3e07f8f36300b
Lair Ortega Dehonor <lair.ortega@gmail.com>
2026-06-26 20:43:42 -0600

    Update postintall.sh

--- postintall.sh
+++ postintall.sh
@@ -29,7 +29,7 @@ docker run hello-world
 echo "Docker dependencies done!"
 
 echo "Install Google Chrome"
-wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget -O chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo apt install ./chrome.deb
 echo "Google Chrome installed!"
 
@@ -37,6 +37,15 @@ echo "Installing VS Code."
 wget -O vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
 sudo apt install ./vscode.deb -y
 echo "VS Code installed!"
+
+echo "Installing Dbeaver."
+wget -O dbeaver.deb 'https://dbeaver.io/files/dbeaver-ce-latest-linux-x86_64.deb'
+sudo apt install ./dbeaver.deb -y
+echo "Dbeaver installed!"
+
+echo "Installing nvm"
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash
+echo "nvm installed!"
 
 echo "System will restart in 20 secs"
 echo "Press \"ctrl+c\" to abort."
